0cb704c40f6d6c897cc9b73de6f9202248412fce
DodyNaufal <dody.naufal@gmail.com>
2025-07-10 23:26:35 +0700

    databases

--- config/db.js
+++ config/db.js
@@ -1,18 +1,12 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const db = mysql.createPool({
-  host: "localhost", // Jika pakai XAMPP, tetap 'localhost'
-  user: "root", // Default user XAMPP
-  password: "", // Default tanpa password
-  database: "lms", // Ganti dengan nama database kamu
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Database connection failed: " + err.stack);
-//     return;
-//   }
-//   console.log("Connected to MySQL database.");
-// });
-
-module.exports = db;
+module.exports = pool;
